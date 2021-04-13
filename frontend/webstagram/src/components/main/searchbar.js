ea8fb4ea9fce0e610674d244ea257350ad167773
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-04-13 13:13:18 -0700

    Made search bar a functional component, might need to make that a class to handle profile

---
+++ frontend/webstagram/src/components/main/searchbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function SearchBar() {
+    return(
+        <div>
+            <nav>
+                <a href="#">Webstagram</a>
+                <input>Search Bar</input>
+                <a href="#">Home</a>
+                <a href="#">Messages</a>
+                <a href="#">Likes</a>
+                <a href="#">Profile</a>
+            </nav>
+        </div>
+    )
+}
+
+export default SearchBar;
