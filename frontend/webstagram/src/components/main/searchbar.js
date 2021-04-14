40d95d5c56b2e2dc3078f35ec807e860a7f846f8
MWeberLambdaweb19 <mack.webb37@gmail.com>
2021-04-14 14:26:28 -0700

    Sidebar skeleton finished, working on postcontainer

--- frontend/webstagram/src/components/main/searchbar.js
+++ frontend/webstagram/src/components/main/searchbar.js
@@ -5,6 +5,7 @@ function SearchBar() {
         <div>
             <nav>
                 <a href="#">Webstagram</a>
+                {/* Makes a search call for users, not items, on keypress */}
                 <input>Search Bar</input>
                 <a href="#">Home</a>
                 <a href="#">Messages</a>
